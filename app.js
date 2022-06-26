d73659e19bf904a8bba2377afb4d1b10f3e384ae
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-06-26 15:42:34 -0500

    refactored

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
+
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -177,13 +178,36 @@ const mockData ={
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw err;
-      console.log('Portfolio complete! Check out the index.html to see the output!"');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+    // fs.writeFile('./dist/index.html', pageHTML, err => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log('Page created! Check out the index.html to see it!"');
+    //   fs.copyFile('./src/style.css', './dist/style.css', err=> {
+    //     if (err) {
+    //       console.log(err);
+    //       return;
+    //     }
+    //     console.log('style sheet copied successfully');
+    //   });
+    // });
+  // });
 
 
 //section 9.1
